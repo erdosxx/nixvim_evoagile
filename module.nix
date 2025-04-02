67d5f72d878d82ece5e515c251863b2771e6fec1
Norel <norel.evoagile@gmail.com>
2025-04-02 21:57:12 +0900

    Fix import for module.

--- module.nix
+++ module.nix
@@ -1,12 +1,13 @@
 # module.nix
 { inputs, ... }: {
-  imports = [
-    inputs.nixvim.homeManagerModules.nixvim
-    ./config # Your existing configuration
-  ];
+  imports = [ inputs.nixvim.homeManagerModules.nixvim ];
 
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
     # Additional NixVim settings can go here
+    imports = [
+      ./config # Your existing configuration
+    ];
   };
 }
