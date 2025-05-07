897c8df6fd68795d95b92b79a36836ae1abc78aa
Norel <norel.evoagile@gmail.com>
2025-05-08 08:14:54 +0900

    Update definition of diagnositc sign according to new api.

--- config/default.nix
+++ config/default.nix
@@ -56,19 +56,16 @@
     ''
       vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
 
-      local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
-      }
-
-      for _, sign in ipairs(signs) do
-      	vim.fn.sign_define(
-      		sign.name,
-      		{ texthl = sign.name, text = sign.text, numhl = "" }
-      	)
-      end
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            Error = "",
+            Warn  = "",
+            Hint  = "",
+            Info  = "",
+          }
+        }
+      })
 
       vim.lsp.handlers["textDocument/hover"] =
         vim.lsp.with(vim.lsp.handlers.hover, {
