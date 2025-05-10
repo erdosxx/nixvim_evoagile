0800c72102a5516dc81b192744c8dc98871229f0
Norel <norel.evoagile@gmail.com>
2025-05-10 23:56:22 +0900

    Set width of vertical toggle term 40. Relative width has an issue with nvimtree.

--- config/plugins/toggleterm.nix
+++ config/plugins/toggleterm.nix
@@ -112,7 +112,7 @@ in {
           if term.direction == "horizontal" then
             return 20
           elseif term.direction == "vertical" then
-            return vim.o.columns * 0.3
+            return 40
           end
         end
       '';
