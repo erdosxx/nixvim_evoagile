0e1e660424534c080785a9bcb2d4417d50ab8ab1
Norel <norel.evoagile@gmail.com>
2026-06-23 17:39:25 +0900

    restore vimtex.

--- config/default.nix
+++ config/default.nix
@@ -30,7 +30,7 @@
     ./plugins/telescope-harpoon.nix
     ./plugins/toggleterm.nix
     (import ./plugins/treesitter.nix {inherit pkgs;})
-    # import ./plugins/vimtex.nix
+    import ./plugins/vimtex.nix
     ./plugins/whichkey.nix
   ];
 
