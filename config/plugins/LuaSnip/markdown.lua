a19823491fe9a14e7ec79011c004a1b305654a56
Norel <norel.evoagile@gmail.com>
2026-07-14 06:13:14 +0900

    Add new luasnip for markdown.

--- config/plugins/LuaSnip/markdown.lua
+++ config/plugins/LuaSnip/markdown.lua
@@ -96,6 +96,10 @@ local snips = {
   ),
   asp({ trig = ";mm", name = "math mode" }, fmta([[$<>$]], { i(1) })),
   asp({ trig = ";ms" }, { t("(X,𝒜,\\mu)") }),
+  asp(
+    { trig = ";rm", name = "mathrm", dscr = "Use roman charater" },
+    fmta([[\mathrm{<>} <>]], { i(1), i(0) })
+  ),
   asp({ trig = ";ps" }, { t("(\\Omega,𝒜,\\mathbb{P})") }),
   asp({ trig = ";sa" }, { t("$\\sigma$-algebra ") }),
   asp({ trig = ";sf" }, { t("$\\sigma$-finite ") }),
