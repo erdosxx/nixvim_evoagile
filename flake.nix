3983c94026f3e9881e04ee04cd650607c464ad3c
Norel <norel.evoagile@gmail.com>
2026-09-14 18:45:15 +0900

    Restore not to use follows in nixvim.

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
       url = "github:nix-community/nixvim";
       # url =
       #   "github:nix-community/nixvim?rev=41844750e55f17b1385d5b09ca7ade5f11f49506";
-      inputs.nixpkgs.follows = "nixpkgs-small-fix";
+      # inputs.nixpkgs.follows = "nixpkgs-small-fix";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs-small-fix.url = "github:nixos/nixpkgs?rev=f496248152e1ad8c61b59a6739cc499b447168e4";
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
